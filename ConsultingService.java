3a3f195597c1f746ac07b530eecb8ef6770f14a0
etud0377 <85850475+etud0377@users.noreply.github.com>
2021-10-06 13:01:54 -0400

    Update ConsultingService.java

--- ConsultingService.java
+++ ConsultingService.java
@@ -1,14 +1,5 @@
 
-//*********************************************************
 
-/*
- * In this file, do the following things:
- * 1) Modify the class so that all ConsultingService objects must use the Sellable interface
- * 2) Create a constructor that takes in a consulting service name, consulting service cost per hour,
- *    and a consulting service number of hours
- * 3) Override the getCost() method and return the result of multiplying the consulting service cost per hour by
- *    the consulting service number of hours
- */
  
 
 public class ConsultingService implements Sellable {
